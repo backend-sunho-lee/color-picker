60bb5c6fd5dfcade9d6c2fd29bbc5658e8ea062a
sssunho <sh293zz@gmail.com>
2017-08-08 23:52:16 +0900

    pick most used colors.

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
-# color_picker
-pick most used colors
+# Color Picker
+pick most used colors.
+
+The file format does not matter. At least jpg and png are possible.
+
+사진에서 많이 사용된 색을 골라줍니다. 5개까지 제대로 나오는 듯?
+
+코드를 작성하는 것보다 라이브러리 다운로드 받는게 더더더더더 힘들었습니다..
+
+
+## Requirements
+- sklearn
+- numpy
+- matplotlib
+- opencv
+
+
+## How to use
+### use with Flask
+1. run pick_colors_with_flask.py
+
+2. using Tool like PostMan
+```
+POST /pick/colors/<int:num>
+Host: localhost:5000
+Content-Type: multipart/form-data
+Kye: img
+Values: file, your image
+```
+I recommend that you only include `num` up to 5
+
+3. get colors!
+```
+{
+    "colors": [
+        {
+            "rgb": [ 30, 25, 24 ],
+            "hex": "#1e1918"
+        }
+}
+```
+
+### use with argparse
+```
+python pick_colors_with_argparse.py --image images/your_image.png --clusters 3
+```
+
+
+## Reference
+- [OpenCV and Python K-Means Color Clustering](http://www.pyimagesearch.com/2014/05/26/opencv-python-k-means-color-clustering/)
+- [Ubuntu 16.04에 opencv_contrib 포함하여 OpenCV 3.2 설치](http://webnautes.tistory.com/1030)
---
+++ pick_colors_with_flask.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, make_response, jsonify
+
+from sklearn.cluster import KMeans
+import cv2
+import numpy as np
+import utils
+
+'''
+export MPLBACKEND="pdf" 또는 "Agg"
+환경변수 반드시 설정해주고 실행시키기!
+'''
+
+app = Flask(__name__)
+
+@app.route('/pick/colors/<int:num>', methods=['POST'])
+def colorPicker(num):
+    _img = request.files.get('img', None)
+
+    #: load the image and convert it from BGR to RGB so that
+    #: we can dispaly it with matplotlib
+    image = np.asarray(bytearray(_img.read()), dtype='uint8')
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    #: reshape the image to be a list of pixels
+    image = image.reshape((image.shape[0] * image.shape[1], 3))
+
+    #: cluster the pixel intensities
+    clt = KMeans(n_clusters=num)
+    clt.fit(image)
+
+    #: build a histogram of clusters and then create a figure
+    #: representing the number of pixels labeled to each color
+    hist = utils.centroid_histogram(clt)
+    colors, bar = utils.plot_colors(hist, clt.cluster_centers_)
+
+    result = []
+    for color in colors:
+        temp = {}
+        temp['rgb'] = color
+        hexcolor = '#{:02x}{:02x}{:02x}'.format(color[0], color[1], color[2])
+        temp['hex'] = hexcolor
+
+        result.append(temp)
+
+    return make_response(jsonify(colors=result))
+
+if __name__ == '__main__':
+    app.run()
